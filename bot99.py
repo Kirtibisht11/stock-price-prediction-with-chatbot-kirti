b9c5d21c2ccbdd6589328fbe5cc77166515ed39d
Kirti Bisht <144167639+Kirtibisht11@users.noreply.github.com>
2024-11-30 20:29:24 +0530

    Update bot99.py

--- bot99.py
+++ bot99.py
@@ -119,5 +119,14 @@ def predict():
 def home():
     return render_template('combine.html')
 
+# About page route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Contact page route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 if __name__ == "__main__":
     app.run(debug=True)
